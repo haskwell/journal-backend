b170ebb5693cce5cfbba4d9d309d9eced9db5c00
snow <muhbilalchaudhry@gmail.com>
2025-07-30 17:12:13 +0500

    nuked

--- src/db/client.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from 'drizzle-orm/d1'
-import type { Context } from 'hono'
-import * as schema from './schema'
-import { Env } from '../env'
-
-export const getDb = (c: Context<Env>) => drizzle(c.env.DB, { schema })
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono'
-import { entryRoutes } from './routes/entries'
-
-const app = new Hono()
-
-app.route('/journal', entryRoutes)
-
-export default app
