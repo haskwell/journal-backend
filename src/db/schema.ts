ed79b0fe93d5ce8c1ad9e7d45d426decf0670be7
snow <muhbilalchaudhry@gmail.com>
2025-07-31 18:28:54 +0500

    initial schema generated

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,7 @@ import { sql } from "drizzle-orm";
 import { integer, text, sqliteTable, unique } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable('USERS', {
-  userId: integer('USER_ID').primaryKey().notNull(),
+  userId: text('USER_ID').primaryKey().notNull(),
   username: text('USERNAME').notNull().unique(),
   passwordHash: text('PASSWORD_HASH').notNull(),
   email: text('EMAIL_ADDRESS').notNull().unique(),
@@ -10,35 +10,29 @@ export const users = sqliteTable('USERS', {
 });
 
 export const journal = sqliteTable('JOURNAL', {
-  journalId: integer('JOURNAL_ID').notNull().primaryKey(),
-  userId: integer('USER_ID').references(() => users.userId, {onDelete: "cascade"}).notNull(),
+  journalId: text('JOURNAL_ID').notNull().primaryKey(),
+  userId: text('USER_ID').references(() => users.userId, {onDelete: "cascade"}).notNull(),
   journalNumber: integer('JOURNAL_NUMBER').notNull(),
   title: text('TITLE').notNull(),
   dateCreated: text('DATE_CREATED').default(sql`CURRENT_TIMESTAMP`),
-}, (table) => [
-  unique('unique_journal_per_user').on(table.userId, table.journalNumber)
-])
+})
 
 export const pages = sqliteTable('PAGES', {
-  pageId: integer('PAGE_ID').primaryKey().notNull(),
-  journalId: integer('JOURNAL_ID').notNull().references(() => journal.journalId, {onDelete: "cascade"}),
+  pageId: text('PAGE_ID').primaryKey().notNull(),
+  journalId: text('JOURNAL_ID').notNull().references(() => journal.journalId, {onDelete: "cascade"}),
   pageNumber: integer("PAGE_NUMBER").notNull(),
   title: text('TITLE').notNull(),
   content: text('CONTENT').notNull(),
   mood: integer('MOOD').notNull(),
   dateCreated: text('DATE_CREATED').default(sql`CURRENT_TIMESTAMP`),
   dateModified: text('DATE_MODIFIED').default(sql`CURRENT_TIMESTAMP`),
-}, (table) => [
-  unique("unique_page_per_journal").on(table.journalId, table.pageNumber)
-])
+})
 
 export const sharedPages = sqliteTable('SHARED_PAGES', {
-  sharingId: integer('SHARING_ID').primaryKey().notNull(),
-  sharedFromUserId: integer('SHARED_FROM_USER_ID').references(() => users.userId).notNull(),
-  sharedToUserId: integer('SHARED_TO_USER_ID').references(() => users.userId).notNull(),
-  sharedPageId: integer('SHARED_PAGE_ID').references(() => pages.pageId, {onDelete: "cascade"}).notNull(),
+  sharingId: text('SHARING_ID').primaryKey().notNull(),
+  sharedFromUserId: text('SHARED_FROM_USER_ID').references(() => users.userId).notNull(),
+  sharedToUserId: text('SHARED_TO_USER_ID').references(() => users.userId).notNull(),
+  sharedPageId: text('SHARED_PAGE_ID').references(() => pages.pageId, {onDelete: "cascade"}).notNull(),
   isPrivate: integer('IS_PRIVATE').default(1).notNull(),
   dateShared: text('DATE_SHARED').default(sql`CURRENT_TIMESTAMP`),
-}, (table) => [
-  unique('one_share_per_pair').on(table.sharedFromUserId, table.sharedToUserId, table.sharedPageId)
-])
+})
