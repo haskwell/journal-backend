c6bf2a432954d69628fdc2c876037772e525a20c
snow <muhbilalchaudhry@gmail.com>
2025-08-16 01:04:05 +0500

    bug fix

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,7 +6,7 @@ const authRoutes = new Hono();
 authRoutes.post('/login', authHandler.loginHandler);
 authRoutes.post('/register', authHandler.registerHandler);
 authRoutes.post('/logout', authHandler.logout);
-authRoutes.get('/auth/me', authHandler.getCurrentUserHandler);
+authRoutes.get('/me', authHandler.getCurrentUserHandler);
 authRoutes.post('/password-reset-request', authHandler.passwordResetRequestHandler);
 authRoutes.post('/password-reset', authHandler.passwordResetHandler);
 authRoutes.patch('/auth/update-username', authHandler.updateUsernameHandler);
